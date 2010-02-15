2175ea3f14a47e2de6b84501dcd4a9eb63b235a9
Colin Shea <colin@evaryont.me>
2010-02-14 20:56:06 -0500

    Basic template done.

--- pummel.rb
+++ pummel.rb
@@ -1,14 +1,35 @@
 require 'iconv'
-require 'sinatra'
 require 'rss/1.0'
 require 'rss/2.0'
 require 'open-uri'
+require 'sinatra'
 
 @@rss_url = "http://feeds.pinboard.in/rss/u:evaryont/"
 ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
 valid_string = ic.iconv(open(@@rss_url).read << ' ')[0..-2]
 @@rss = RSS::Parser.parse valid_string, false
 
-get '/' do
-    "#{@@rss.channel.title} <a href='#{@@rss.channel.link}'>Go</a>"
+def rss
+    @@rss
 end
+
+get '/' do
+    erb :index
+end
+
+use_in_file_templates!
+
+__END__
+
+@@ index
+<%# Borrowed from http://rubyrss.org %>
+<h4><a href="<%= rss.channel.link %>"><%= rss.channel.title %></a></h4>
+% if rss.channel.date
+<small>Last updated on <%= rss.channel.date.strftime("%d %b %Y") %></small>
+% end
+<p><%= rss.channel.description %></p>
+<ol>
+% rss.channel.items.each do |item|
+    <li><%= item.title %></li>
+%end
+</ol>
