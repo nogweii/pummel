50ba252d84f5e0523e4bf91ae53e6b86c3387a81
Colin Shea <colin@evaryont.me>
2010-02-15 14:14:47 -0500

    Add the ability to refresh the RSS feed.

--- pummel.rb
+++ pummel.rb
@@ -6,10 +6,14 @@ require 'sinatra'
 $: << File.join(File.dirname(__FILE__), 'vendor', 'ruby-oembed', 'lib')
 require 'oembed'
 
+def refresh_rss
+    valid_string = @ic.iconv(open(@@rss_url).read << ' ')[0..-2]
+    @@rss = RSS::Parser.parse valid_string, false
+end
+
 @@rss_url = "http://feeds.pinboard.in/rss/u:evaryont/"
-ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
-valid_string = ic.iconv(open(@@rss_url).read << ' ')[0..-2]
-@@rss = RSS::Parser.parse valid_string, false
+@ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
+refresh_rss()
 
 set :public, File.dirname(__FILE__) + '/public'
 set :views, File.dirname(__FILE__) + '/templates'
@@ -20,4 +24,9 @@ end
 
 get '/' do
     erb :index
+end
+
+get '/refresh' do
+    refresh_rss()
+    redirect :/
 end
